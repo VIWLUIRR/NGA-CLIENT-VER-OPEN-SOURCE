07236530be332e439df5bfde176ad7f5903e46d6
Justwen <yyhshrrr@126.com>
2025-09-22 19:00:39 +0800

    移除老代码

--- nga_phone_base_3.0/src/main/java/gov/anzong/androidnga/activity/MainActivity.java
+++ nga_phone_base_3.0/src/main/java/gov/anzong/androidnga/activity/MainActivity.java
@@ -5,39 +5,24 @@ import android.app.Activity;
 import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import androidx.activity.ComponentActivity;
 import androidx.activity.EdgeToEdge;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
-
-import com.alibaba.android.arouter.facade.Postcard;
-import com.alibaba.android.arouter.launcher.ARouter;
 
 import gov.anzong.androidnga.NgaClientApp;
 import gov.anzong.androidnga.R;
-import gov.anzong.androidnga.arouter.ARouterConstants;
+import gov.anzong.androidnga.activity.compose.drawer.NavigationDrawerFragment;
 import gov.anzong.androidnga.base.util.PermissionUtils;
 import gov.anzong.androidnga.base.util.ThemeUtils;
-import sp.phone.common.User;
-import sp.phone.common.UserManagerImpl;
-import sp.phone.param.ParamKey;
 import sp.phone.theme.ThemeManager;
-import sp.phone.ui.fragment.BaseFragment;
-import sp.phone.ui.fragment.NavigationDrawerFragment;
-import sp.phone.ui.fragment.dialog.UrlInputDialogFragment;
 import sp.phone.ui.fragment.dialog.VersionUpgradeDialogFragment;
-import sp.phone.util.ARouterUtils;
 import sp.phone.util.ActivityUtils;
 
 public class MainActivity extends BaseActivity {
 
     private boolean mIsNightMode;
-
-    private BaseFragment mBoardFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -89,127 +74,19 @@ public class MainActivity extends BaseActivity {
 
     private void initView() {
         FragmentManager fm = getSupportFragmentManager();
-//        mBoardFragment = (BaseFragment) fm.findFragmentByTag(NavigationDrawerFragment.class.getSimpleName());
-        //if (mBoardFragment == null) {
-        //    mBoardFragment = new NavigationDrawerFragment();
-        //    fm.beginTransaction().replace(android.R.id.content, mBoardFragment, NavigationDrawerFragment.class.getSimpleName()).commit();
-        //}
-
-        Fragment fragment = fm.findFragmentByTag(gov.anzong.androidnga.activity.compose.drawer.NavigationDrawerFragment.class.getSimpleName());
+        Fragment fragment = fm.findFragmentByTag(NavigationDrawerFragment.class.getSimpleName());
         if (fragment == null) {
-            fm.beginTransaction().replace(android.R.id.content, new gov.anzong.androidnga.activity.compose.drawer.NavigationDrawerFragment(), NavigationDrawerFragment.class.getSimpleName()).commit();
+            fm.beginTransaction().replace(android.R.id.content, new NavigationDrawerFragment(), NavigationDrawerFragment.class.getSimpleName()).commit();
         }
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_option_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action buttons
-        switch (item.getItemId()) {
-            case R.id.menu_setting:
-                startSettingActivity();
-                break;
-            case R.id.menu_bookmark:
-                startFavoriteTopicActivity();
-                break;
-            case R.id.menu_msg:
-                startMessageActivity();
-                break;
-            case R.id.menu_post:
-                startPostActivity(false);
-                break;
-            case R.id.menu_reply:
-                startPostActivity(true);
-                break;
-            case R.id.menu_about:
-                aboutNgaClient();
-                break;
-            case R.id.menu_search:
-                startSearchActivity();
-                break;
-            case R.id.menu_forward:
-                new UrlInputDialogFragment().show(getSupportFragmentManager());
-                break;
-            case R.id.menu_gun:
-                startNotificationActivity();
-                break;
-            case R.id.menu_download:
-                startActivity(new Intent(this, TopicCacheActivity.class));
-                break;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-        return true;
-    }
-
-    private void startSearchActivity() {
-        ARouter.getInstance()
-                .build(ARouterConstants.ACTIVITY_SEARCH)
-                .navigation(this);
-    }
-
-    private void startMessageActivity() {
-        ARouterUtils
-                .build(ARouterConstants.ACTIVITY_MESSAGE_LIST)
-                .navigation(this);
-    }
-
-    private void aboutNgaClient() {
-        // new AboutClientDialogFragment().show(getSupportFragmentManager());
-        startActivity(new Intent(this, AboutActivity.class));
-    }
-
-    private void startSettingActivity() {
-        Intent intent = new Intent();
-        intent.setClass(MainActivity.this, SettingsActivity.class);
-        startActivityForResult(intent, ActivityUtils.REQUEST_CODE_SETTING);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == ActivityUtils.REQUEST_CODE_SETTING && resultCode == Activity.RESULT_OK) {
             recreate();
-        } else {
-            if (mBoardFragment != null) {
-                mBoardFragment.onActivityResult(requestCode, resultCode, data);
-            }
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
-
-    private void startNotificationActivity() {
-        ARouterUtils
-                .build(ARouterConstants.ACTIVITY_NOTIFICATION)
-                .navigation(this);
-    }
-
-    // NPE问题
-    private void startPostActivity(boolean isReply) {
-        User user = UserManagerImpl.getInstance().getActiveUser();
-        String userName = user != null ? user.getNickName() : "";
-        int uid = user != null ? Integer.parseInt(user.getUserId()) : 0;
-        Postcard postcard = ARouterUtils
-                .build(ARouterConstants.ACTIVITY_TOPIC_LIST)
-                .withInt(ParamKey.KEY_AUTHOR_ID, uid)
-                .withString(ParamKey.KEY_AUTHOR, userName);
-        if (isReply) {
-            postcard.withInt(ParamKey.KEY_SEARCH_POST, 1);
-        }
-        postcard.navigation(this);
-    }
-
-    private void startFavoriteTopicActivity() {
-        ARouterUtils
-                .build(ARouterConstants.ACTIVITY_TOPIC_LIST)
-                .withInt(ParamKey.KEY_FAVOR, 1)
-                .navigation(this);
-    }
-
 
 }
